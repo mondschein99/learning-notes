1f01abb1bbda51097d677633f9f58cd9bf5cd09d
mondschein99 <jadehuang0218@gmail.com>
2025-04-22 00:15:31 +0800

    Update 53_Maximum_Subarray.py

--- 53_Maximum_Subarray.py
+++ 53_Maximum_Subarray.py
@@ -1,1 +1,20 @@
+#Question: Given an integer array nums, find the subarray with the largest sum, and return its sum
 
+class Solution(object):
+    def maxSubArray(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        max_list = [0]
+        while(nums):
+            num = nums.pop(-1)
+            max_list.append(max(num, num + max_list[-1]))
+        return max(max_list[1:])
+      
+#Basic thought: Kadane’s Algorithm
+'''
+Key decision:
+If adding the current number to current_sum makes it better (than current number), continue.
+If it makes it worse, drop the old subarray and start fresh from the current number.
+'''
