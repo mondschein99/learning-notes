22e05ee1278f1a00c4d495c6c5fc5450c72689c2
mondschein99 <jadehuang0218@gmail.com>
2025-04-22 12:38:08 +0800

    Update 121_Best_Time_To_Buy_And_Sell_Stock.py

--- 121_Best_Time_To_Buy_And_Sell_Stock.py
+++ 121_Best_Time_To_Buy_And_Sell_Stock.py
@@ -2,10 +2,23 @@
 #          You want to maximize your profit by choosing a single day to buy one stock and choosing a different day in the future to sell that stock.
 #          Return the maximum profit you can achieve from this transaction. If you cannot achieve any profit, return 0.
 
+class Solution(object):
+    def maxProfit(self, prices):
+        """
+        :type prices: List[int]
+        :rtype: int
+        """
+        profit = 0
+        buy = prices[0]
 
-
+        for price in prices:
+            if price - buy > profit:
+                profit = price - buy
+            if price < buy:
+                buy = price
+        return profit
 
 '''
 Key decision:
-
+Keep tracking the minimum price
 '''
