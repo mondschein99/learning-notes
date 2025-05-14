724fd5125a72ad471faa2169196c0ba6abc7ce30
mondschein99 <jadehuang0218@gmail.com>
2025-05-15 02:13:17 +0800

    Create 226_Invert_Binary_Tree.py

---
+++ 226_Invert_Binary_Tree.py
@@ -0,0 +1,30 @@
+#Question: Given the root of a binary tree, invert the tree, and return its root.
+
+'''
+key point:
+recursive loop
+'''
+
+
+#Reference
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+from collections import deque
+class Solution(object):
+    def invertTree(self, root):
+        """
+        :type root: Optional[TreeNode]
+        :rtype: Optional[TreeNode]
+        """
+        if not root:
+            return None
+        root.left, root.right = root.right, root.left
+        self.invertTree(root.left)
+        self.invertTree(root.right)
+        return root
+
+
