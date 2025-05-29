c5360e25838fcf78725855112d064f89ca0581d4
mondschein99 <jadehuang0218@gmail.com>
2025-05-29 14:41:27 +0800

    Create 704_Binary_Search.py

---
+++ 704_Binary_Search.py
@@ -0,0 +1,21 @@
+#Question:  Given an array of integers nums which is sorted in ascending order, and an integer target, write a function to search target in nums. 
+#           If target exists, then return its index. Otherwise, return -1.
+#           You must write an algorithm with O(log n) runtime complexity.
+
+
+'''Solution'''
+class Solution(object):
+    def search(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: int
+        """
+        dic = {}
+        index = 0
+        for num in nums:
+            dic[num] = index
+            index += 1
+        if target not in dic:
+            return -1
+        return dic[target]
