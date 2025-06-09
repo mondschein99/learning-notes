e841baa9bb52e86e2bfa48d086e63be6afc152f5
mondschein99 <jadehuang0218@gmail.com>
2025-06-09 16:05:12 +0800

    Update 34_Find_First_and_Last_Position_of_Element_in_Sorted_Array.py

--- 34_Find_First_and_Last_Position_of_Element_in_Sorted_Array.py
+++ 34_Find_First_and_Last_Position_of_Element_in_Sorted_Array.py
@@ -62,3 +62,38 @@ For the modified version, the running time is O(logn).
 Considering numbers approach from both sides, the number of times numbers can be devided is
 log2(n).
 '''
+
+#Modified Version2:
+def lower_bound(nums, target):
+    left = 0
+    right = len(nums) - 1
+    while left <= right:
+        mid = (left + right) // 2
+        if nums[mid] <target:
+            left = mid + 1
+        else:
+            right = mid - 1
+    return left
+
+class Solution(object):
+    def searchRange(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: List[int]
+        """
+        start = lower_bound(nums, target)
+        if start == len(nums) or nums[start] != target:
+            return [-1, -1]
+        end = lower_bound(nums, target + 1) - 1
+        return [start, end]
+
+
+        
+
+
+
+
+
+
+        
